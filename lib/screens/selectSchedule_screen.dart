d1271fa590ae08d4ce3d01a6c2e5c305d17538c5
hyemimi <hyemi7375@gmail.com>
2024-08-13 17:20:17 +0900

    calender screen

--- lib/screens/selectSchedule_screen.dart
+++ lib/screens/selectSchedule_screen.dart
@@ -57,63 +57,96 @@ class _SelectScheduleScreenState extends State<SelectScheduleScreen> {
         body: SafeArea(
             child: Padding(
                 padding: const EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    SizedBox(height: 20),
-                    Image.asset(
-                      'assets/calender.png',
-                      width: double.infinity,
-                      height: 200,
-                      fit: BoxFit.cover,
-                    ),
-                    Expanded(
-                        child: TableCalendar(
-                      firstDay: DateTime.now(),
-                      lastDay: DateTime.utc(2025, 12, 31),
-                      focusedDay: _focusedDay,
-                      calendarFormat: CalendarFormat.month,
-                      selectedDayPredicate: (day) =>
-                          isSameDay(_selectedDay, day),
-                      onDaySelected: _onDaySelected,
-                      rangeStartDay: _rangeStart,
-                      rangeEndDay: _rangeEnd,
-                      onRangeSelected: _onRangeSelected,
-                      rangeSelectionMode: RangeSelectionMode.toggledOn,
-                      calendarStyle: CalendarStyle(
-                        rangeStartTextStyle:
-                            TextStyle(color: Colors.black), // 범위 시작일 글자색
-                        rangeEndTextStyle: TextStyle(color: Colors.black),
-                        rangeStartDecoration: BoxDecoration(
-                          color: Color(0xffE6F7B4),
-                          shape: BoxShape.circle,
-                        ),
-                        rangeEndDecoration: BoxDecoration(
-                          color: Color(0xffE6F7B4),
-                          shape: BoxShape.circle,
-                        ),
-                        rangeHighlightColor: Color(0xffDFF797),
-                        selectedDecoration: BoxDecoration(
-                          color: Color(0xffF7ECB4),
-                          shape: BoxShape.circle,
-                        ),
-                        selectedTextStyle: TextStyle(color: Colors.black),
-                        todayDecoration: BoxDecoration(
-                          color: Color(0xffF7ECB4),
-                          shape: BoxShape.circle,
-                        ),
-                        todayTextStyle: TextStyle(color: Colors.black),
+                child: Column(children: [
+                  SizedBox(height: 20),
+                  Image.asset(
+                    'assets/calender.png',
+                    width: double.infinity,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                  Expanded(
+                      child: TableCalendar(
+                    firstDay: DateTime.now(),
+                    lastDay: DateTime.utc(2025, 12, 31),
+                    focusedDay: _focusedDay,
+                    calendarFormat: CalendarFormat.month,
+                    selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+                    onDaySelected: _onDaySelected,
+                    rangeStartDay: _rangeStart,
+                    rangeEndDay: _rangeEnd,
+                    onRangeSelected: _onRangeSelected,
+                    rangeSelectionMode: RangeSelectionMode.toggledOn,
+                    calendarStyle: CalendarStyle(
+                      rangeStartTextStyle:
+                          TextStyle(color: Colors.black), // 범위 시작일 글자색
+                      rangeEndTextStyle: TextStyle(color: Colors.black),
+                      rangeStartDecoration: BoxDecoration(
+                        color: Color(0xffC0F4B4),
+                        shape: BoxShape.circle,
                       ),
-                      headerStyle: HeaderStyle(
-                        titleCentered: true,
-                        formatButtonVisible: false,
+                      rangeEndDecoration: BoxDecoration(
+                        color: Color(0xffC0F4B4),
+                        shape: BoxShape.circle,
                       ),
-                    )),
-                    SizedBox(height: 20),
-                    Text(
-                      'Selected range: ${_rangeStart?.toLocal()} - ${_rangeEnd?.toLocal()}',
-                      style: TextStyle(fontSize: 16),
+                      rangeHighlightColor: Color(0xffC0F4B4),
+                      selectedDecoration: BoxDecoration(
+                        color: Color(0xffF7ECB4),
+                        shape: BoxShape.circle,
+                      ),
+                      selectedTextStyle: TextStyle(color: Colors.black),
+                      todayDecoration: BoxDecoration(
+                        color: Color(0xffF7ECB4),
+                        shape: BoxShape.circle,
+                      ),
+                      todayTextStyle: TextStyle(color: Colors.black),
                     ),
-                  ],
-                ))));
+                    headerStyle: HeaderStyle(
+                      titleCentered: true,
+                      formatButtonVisible: false,
+                    ),
+                  )),
+                  Container(
+                      height: 80,
+                      padding: const EdgeInsets.all(14),
+                      decoration: BoxDecoration(color: Color(0xffE6F7B4)),
+                      child: Center(
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Column(
+                                children: [
+                                  Text('결제 금액'),
+                                  Text('총 $price 만원',
+                                      style: TextStyle(
+                                        fontSize: 18.0, // 폰트 크기를 24로 설정
+                                      ))
+                                ],
+                              ),
+                              Container(
+                                width: 1, // 수직선의 너비
+                                height: 100, // 수직선의 높이
+                                color: Color(0xff928B7C), // 수직선의 색상
+                              ),
+                              TextButton(
+                                  onPressed: () => Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                SelectScheduleScreen()),
+                                      ),
+                                  child: Text(
+                                    "결제하기",
+                                    style: TextStyle(
+                                      fontSize: 18.0, // 폰트 크기를 24로 설정
+                                    ),
+                                  ))
+                            ]),
+                      ))
+                ]))));
   }
 }
+
+//Text(
+//'Selected range: ${_rangeStart?.toLocal()} - ${_rangeEnd?.toLocal()}',
+//style: TextStyle(fontSize: 16),
+//),
